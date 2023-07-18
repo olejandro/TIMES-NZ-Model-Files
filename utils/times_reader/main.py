f0f0d2e00a23c789649d09ac650fd320a210f64c
Will Catton <will.catton@eeca.govt.nz>
2023-07-18 14:59:28 +1200

    Sort tables

--- utils/times_reader/main.py
+++ utils/times_reader/main.py
@@ -108,6 +108,7 @@ def output_raw_tables_from_xl(
             for f in input_files:
                 result = excel.extract_tables(f)
                 raw_tables.extend(result)
+
         pickle.dump(raw_tables, open(pickle_file, "wb"))
 
     print(
@@ -382,7 +383,7 @@ def strip_filename_prefix(table, prefix):
 
 def dump_tables_modified(tables: List, filename: str, prefix: str) -> List:
     # Modify the filename in each table
-    tables_modified = [strip_filename_prefix(t, prefix) for t in tables]
+    tables_modified = [strip_filename_prefix(t, prefix) for t in tables.sort()]
     return dump_tables(tables_modified, filename)
 
 
